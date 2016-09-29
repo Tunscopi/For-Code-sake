78baae81fd49f9c6b8454f9755b5d653c8ed79c5
Tunscopi <ayotundeodejayi@yahoo.com>
2016-09-29 01:36:04 -0400

    fixed bug using floor function

--- Implementation/Repeated String.cpp
+++ Implementation/Repeated String.cpp
@@ -1,6 +1,7 @@
 
 #include <iostream>
 #include <string>
+#include <math.h>
 using namespace std;
 
 int main() {
@@ -17,7 +18,7 @@ int main() {
             if (s[i] == 'a') a_count++;
         } 
 
-        result = n*a_count/s.length();
+        result = floor(n/s.length())*a_count;
 
         if (n%s.length() > 0) {
             for (int i=0;i<n%s.length();i++) {
