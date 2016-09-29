bf68aed117ae63d88755f2b43bd5ecbe89071c29
Tunscopi <ayotundeodejayi@yahoo.com>
2016-09-28 22:18:44 -0400

    Advantage of this succinct soln. is less space complexity

--- Implementation/Bon Appetit.cpp
+++ Implementation/Bon Appetit.cpp
@@ -7,11 +7,11 @@ int main() {
     int n,k,fair_charge  = 0;
     double sum, b_charge = 0.0;
     cin >> n >> k;
-    float amounts[n];
+    float amount;
 
         for (int i=0; i<n; i++) {
-            cin >> amounts[i];
-            i==k? :sum += amounts[i]/2;
+            cin >> amount;
+            i==k? :sum += amount/2;
         } 
 
         cin >> b_charge;
