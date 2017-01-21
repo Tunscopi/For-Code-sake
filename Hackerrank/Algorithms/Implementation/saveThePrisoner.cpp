1ef12ff766b2a0221ce0303cb79c8c48a0a8b09d
Tunscopi <ayotundeodejayi@yahoo.com>
2017-01-21 17:05:51 -0500

    fixed bug

--- Hackerrank/Algorithms/Implementation/saveThePrisoner.cpp
+++ Hackerrank/Algorithms/Implementation/saveThePrisoner.cpp
@@ -2,13 +2,17 @@
 
 int main()
 {
-    int t,n,m,s;
+    int no_tests,n,m,s;
     int id_no;
-    std::cin >> t >> n >> m >> s;
+    std::cin >> no_tests;
 
 
-    for (int i=0; i<t; i++) {
+    for (int i=0; i<no_tests; i++) {
+        std::cin >> n >> m >> s;
+
         id_no = (m+s-1)%n; 
+        if (id_no == 0)
+            id_no = n;
 
         std::cout << id_no << "\n"; 
     }
